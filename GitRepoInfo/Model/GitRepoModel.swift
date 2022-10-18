76a47654a27ad9a2264dafb5725631b6d40b08c7
hastiranjkesh <hasti.ranjkesh@gmail.com>
2022-10-18 15:39:27 +0200

    Add a decodable struct as GitRepoModel

---
+++ GitRepoInfo/Model/GitRepoModel.swift
@@ -0,0 +1,25 @@
+//
+//  GitRepoModel.swift
+//  GitRepoInfo
+//
+//  Created by hasti ranjkesh on 10/18/22.
+//
+
+import Foundation
+
+struct GitRepoModel: Decodable {
+    let fullName: String
+    let language: String
+    let forks: Int
+    let openIssuesCount: Int
+    let stargazersCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case language = "language"
+        case forks = "forks"
+        case openIssuesCount = "open_issues_count"
+        case stargazersCount = "stargazers_count"
+    }
+}
+
